a4da8c1ba69a759fc787d57c2a3ba183663d4b4c
Bryan VALCASARA <brvalcas@e3r7p20.42.fr>
2018-04-04 13:49:04 +0200

    Ajout du 4eme retry avec la correction de norme

--- ex27/display_file.c
+++ ex27/display_file.c
@@ -6,7 +6,7 @@
 /*   By: brvalcas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/03 15:01:29 by brvalcas          #+#    #+#             */
-/*   Updated: 2018/04/03 18:49:05 by brvalcas         ###   ########.fr       */
+/*   Updated: 2018/04/04 13:42:39 by brvalcas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_display_file(int fd)
 	char	buf[BUFFER + 1];
 	int		ret;
 
-	while((ret = read(fd, buf, BUFFER)) != (-1 && 0))
+	while ((ret = read(fd, buf, BUFFER)) != (-1 && 0))
 	{
 		buf[ret] = '\0';
 		ft_putstr(buf);
